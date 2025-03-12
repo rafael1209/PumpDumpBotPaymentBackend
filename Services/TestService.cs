10e5c7f4c7fc3a5a7eb388ef4fc163ce6e4e18d8
rafael1209 <mr.chasmanrafael@gmail.com>
2025-03-12 16:40:02 +0200

    Update deployment configuration and remove obsolete files
    - Enhanced `PumpDumpBotPaymentBackend20250312140913.yml` for .NET Core app deployment, including API import to Azure API Management.
    - Removed `apis1.arm.json`, which defined the API Management service.
    - Updated `serviceDependencies.PumpDumpBotPaymentBackend20250312140913.json` to eliminate dependency on the removed API.
    - Modified `TestService.cs` to return the current date and time.
    - Added new `profile.arm.json` for Azure resource deployment template, defining App Service and App Service Plan.

--- Services/TestService.cs
+++ Services/TestService.cs
@@ -7,17 +7,7 @@ namespace PumpDumpBotPaymentBackend.Services
     {
         public string Test()
         {
-            string _apiKey = configuration.GetValue<string>("Cryptocloud:ApiKey") ??
-                                              throw new Exception("JCryptocloud:ApiKey not found");
-            string _shopId = configuration.GetValue<string>("Cryptocloud:ShopId") ??
-                                              throw new Exception("Cryptocloud:ShopId not found");
-            string _secretKey = configuration.GetValue<string>("Cryptocloud:SecretJwtKey") ??
-                                throw new Exception("JwtSettings:SecretKey not found");
-            string client = configuration.GetValue<string>("MongoDb:ConnectionString");
-            string client1 = configuration.GetValue<string>("MongoDb:Name");
-
-            return _apiKey + "KEY\n" + _shopId + "SHOP_ID\n" + _secretKey + "SECRETE\n" + client + "CLIENT\n" +
-                   client1 + "CLIENT1\n";
+            return $"{DateTime.Now}";
         }
     }
 }
